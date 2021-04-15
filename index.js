d800a65d4a9c2432f8e7d890a85faa1c5cb00cd0
unknown <gabrielriosaccioli@gmail.com>
2021-04-14 22:24:56 -0300

    Front-End Finalizado

--- index.js
+++ index.js
@@ -12,24 +12,24 @@ app.use(bodyParser.urlencoded({
 app.engine('html', require('ejs'). renderFile)
 app.set('view engine', 'html')
 app.use('/public', express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, '/views'))
+app.set('views', path.join(__dirname, '/pages'))
 
 
-app.get('/', (req, res) => {
-    console.log(req.query)
-
+app.get('/',(req,res)=>{
+    
     if(req.query.busca == null){
-        res.send('Home')
+        res.render('home',{});
     }else{
-        res.send('Você buscou: ' + req.query.busca)
+        res.render('busca',{});
     }
+
+});
+
+app.get('/:slug',(req,res)=>{
+    //res.send(req.params.slug);
+    res.render('single',{});
 })
 
-app.get('/:slug', (req, res)=>{
-    res.send(req.params.slug)
-})
-
-
-app.listen(5000, ()=>{
-    console.log('Server Rodando')
+app.listen(5000,()=>{
+    console.log('server rodando!');
 })
