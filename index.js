5518ff006c0bedbf6901209ab2b9a437a72df520
unknown <gabrielriosaccioli@gmail.com>
2021-04-20 23:25:01 -0300

    Pegando dados dinamicamente via MongoDB

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const bodyParser = require('body-parser')
+const Posts = require('./posts.js')
 
 const app = express()
 
 
-mongoose.connect('mongodb+srv://root:root@cluster0.u5exk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then(function(){
+
+
+mongoose.connect('mongodb+srv://root:root@cluster0.u5exk.mongodb.net/dankicode?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then(function(){
     console.log('Conectado com Sucesso')
 }).catch(function(err){
     console.log(err.message)
@@ -26,7 +29,22 @@ app.set('views', path.join(__dirname, '/pages'))
 app.get('/',(req,res)=>{
     
     if(req.query.busca == null){
-        res.render('home',{});
+        //puxa do banco todos os dados na ordem q foi adicionada
+        Posts.find({}).sort({'_id': -1}).exec(function(err,posts){   
+            //console.log(posts[0])
+            posts = posts.map(function(val){
+                return { 
+                    titulo: val.titulo,
+                    conteudo: val.conteudo,
+                    descricaoCurta: val.conteudo.substr(0,99),
+                    imagem: val.imagem,
+                    slug: val.slug,
+                    categoria: val.categoria
+                }
+            })
+            res.render('home',{posts: posts});
+        })
+        
     }else{
         res.render('busca',{});
     }
